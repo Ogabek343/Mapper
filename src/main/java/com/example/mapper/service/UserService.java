0d77b8df38a30b87e45faf02a0149a42944aa7ca
Ogabek <khamdamov343@gmail.com>
2023-05-05 13:32:48 +0500

    lsdsfds

--- src/main/java/com/example/mapper/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper.service;
-
-import com.example.mapper.model.dto.BaseResponse;
-import com.example.mapper.model.dto.UserRequest;
-import com.example.mapper.model.dto.UserResponse;
-
-public interface UserService {
-    BaseResponse<UserResponse> send(UserRequest request);
-}
