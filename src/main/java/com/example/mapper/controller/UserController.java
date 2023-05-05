d5d6cef2507fd6469d74749d9ca25e2a1a84fbca
Ogabek <khamdamov343@gmail.com>
2023-05-05 15:49:20 +0500

    hrrgr

--- src/main/java/com/example/mapper/controller/UserController.java
+++ src/main/java/com/example/mapper/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.mapper.model.dto.UserResponse;
 import com.example.mapper.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ import static com.example.mapper.model.constants.Paths.BASE_API_PATH;
 public record UserController(UserService service) {
 
     @PostMapping("/send")
-    public ResponseEntity<BaseResponse<UserResponse>> send(UserRequest request){
+    public ResponseEntity<BaseResponse<UserResponse>> send(@RequestBody UserRequest request){
         return ResponseEntity.ok().body(service.send(request));
     }
 
--- src/main/java/com/example/mapper/model/dto/BaseResponse.java
+++ src/main/java/com/example/mapper/model/dto/BaseResponse.java
@@ -1,7 +1,12 @@
 package com.example.mapper.model.dto;
 
+import lombok.Data;
+
+@Data
 public class BaseResponse<T> {
+    private T response;
 
     public BaseResponse(T userResponse) {
+        this.response = userResponse;
     }
 }
